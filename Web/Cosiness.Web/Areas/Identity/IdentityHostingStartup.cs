6b25f1f23a7eb15a8007269554d4025e45515b3f
Denica Atanasova <d.d.atanasova@gmail.com>
2021-04-15 14:54:37 +0300

    Add ASP.NET Core Web project with scaffolded identity.

---
+++ Web/Cosiness.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Cosiness.Data;
+using Cosiness.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Cosiness.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Cosiness.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
