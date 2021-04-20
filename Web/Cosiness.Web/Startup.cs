3d3c660845572088dea22c2c1e38747dd844feca
Denica Atanasova <d.d.atanasova@gmail.com>
2021-04-20 11:46:48 +0300

    Register area routing.

--- Web/Cosiness.Web/Startup.cs
+++ Web/Cosiness.Web/Startup.cs
@@ -92,6 +92,10 @@ namespace Cosiness.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "area",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
 
